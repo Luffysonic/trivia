e0724eaa1a8a6f158ea6db947cf79472820985e3
Ricky Khao <khaoricky@gmail.com>
2023-03-04 14:59:06 +0100

    feat(game.spec): ajout test de WrongAnswer

--- test/game.spec.js
+++ test/game.spec.js
@@ -163,4 +163,28 @@ describe('Test Game', function () {
 		let Win = gametest.didPlayerWin();
 		expect(Win).toBe(false);
 	});
+
+	it("Vérification que si le joueur a mal répondu envoie dans penaltyBox et qu'on est au prochain joueur", function () {
+		let gametest = new game.Game();
+		gametest.add('Ricky');
+		gametest.add('Gabriel');
+
+		gametest.wrongAnswer();
+		let inPenaltyBox = gametest.players[0].isInPenaltyBox();
+		let currentPlayerName = gametest.getCurrentPlayer().getName();
+		expect(inPenaltyBox).toBe(true);
+		expect(currentPlayerName).toBe('Gabriel');
+	});
+
+	it("Vérification que si le joueur a mal répondu envoie dans penaltyBox et qu'on est au prochain joueur cas reviens sur premier", function () {
+		let gametest = new game.Game();
+		gametest.add('Ricky');
+		gametest.add('Gabriel');
+		gametest.currentPlayerIndex = 1;
+		gametest.wrongAnswer();
+		let inPenaltyBox = gametest.players[1].isInPenaltyBox();
+		let currentPlayerName = gametest.getCurrentPlayer().getName();
+		expect(inPenaltyBox).toBe(true);
+		expect(currentPlayerName).toBe('Ricky');
+	});
 });
